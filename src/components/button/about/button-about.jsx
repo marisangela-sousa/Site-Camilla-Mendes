387ac1402446d4bc95ce5c3508abc85a6bbccfa1
Marisangela <marisangelalima110@gmail.com>
2023-10-09 20:59:58 -0300

    Criei o botão about

---
+++ src/components/button/about/button-about.jsx
@@ -0,0 +1,13 @@
+import "../../../styles/styles.css";
+
+const ButtonAbout = () => {
+    return(
+        <div>
+            <button id="button">
+                <p id="button-text">ABOUT</p>
+            </button>
+        </div>
+    )
+}
+
+export default ButtonAbout
---
+++ src/pages/main/main.jsx
@@ -0,0 +1,11 @@
+import ButtonAbout from "../../components/button/about/button-about"
+
+const Main = () =>{
+    return(
+        <div>
+            <ButtonAbout/>
+        </div>
+    )
+}
+
+export default Main
