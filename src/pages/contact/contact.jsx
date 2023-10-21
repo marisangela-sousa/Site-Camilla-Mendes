77c7ea521febebb817a2c6e17229fff3af3f8708
Marisangela <marisangelalima110@gmail.com>
2023-10-21 09:24:42 -0300

    I added almost all the elements of the Contact Page

--- src/pages/contact/contact.jsx
+++ src/pages/contact/contact.jsx
@@ -1,7 +1,21 @@
+import imgcontactpage from "./../../assets/img/imgcontactpage.jpg"
+
+import "./contact.css"
+
 const ContactPage = () => {
     return (
-        <div>
-            Conteudo
+        <div className="ContactPageContainer">
+            <img className="Image" src={imgcontactpage} alt="A girl of black hair and a black blouse looking for the camera" />
+            <div className="TypesOfContactContainer">
+                <div className="ContactsContainer">
+                    <div className="TypeOfContact">CAMILAMENDES</div>
+                    <div className="TypeOfContact">CAMILAMENDES</div>
+                </div>
+                <div className="ContactsContainer">
+                    <div className="TypeOfContact">CAMILAMENDES</div>
+                    <div className="TypeOfContact">CAMILAMENDES</div>
+                </div>
+            </div>
         </div>
     )
 }
